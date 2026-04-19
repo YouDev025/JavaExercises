0df7c43a0c450757f403447e0013b0cec6695e3a
YOUSSEF ADARDOUR <youssefredmi599@gmail.com>
2026-04-19 14:06:07 +0100

    add:practice/src/basics/Patterns.java

---
+++ practice/src/basics/Patterns.java
@@ -0,0 +1,84 @@
+package basics;
+import java.util.Scanner;
+
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        // 1. Right Triangle
+        System.out.println("\nRight Triangle:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 2. Inverted Triangle
+        System.out.println("\nInverted Triangle:");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 3. Pyramid
+        System.out.println("\nPyramid:");
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // ⭐ Challenge 1: Diamond
+        System.out.println("\nDiamond:");
+        // upper part
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower part
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // ⭐ Challenge 2: Pascal’s Triangle
+        System.out.println("\nPascal's Triangle:");
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+
+            // spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // numbers
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
