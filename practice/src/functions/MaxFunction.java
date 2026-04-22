10b924aed7fc9d90dd3912b0c2ceb6593d529269
YOUSSEF ADARDOUR <youssefredmi599@gmail.com>
2026-04-22 11:56:03 +0100

    Add:practice/src/functions/MaxFunction.java

--- practice/src/functions/MaxFunction.java
+++ practice/src/functions/MaxFunction.java
@@ -1,5 +1,25 @@
 package functions;
-import  java.util.Scanner;
-public class MaxExample {
-    pubil
+import java.util.Scanner;
+
+public class MaxFunction {
+
+    public static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int result = max(a, b);
+
+        System.out.println("Maximum is: " + result);
+
+        sc.close();
+    }
 }
