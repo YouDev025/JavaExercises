277f32b9895635ede1efd8a1206dc992ab65bcb1
YOUSSEF ADARDOUR <youssefredmi599@gmail.com>
2026-04-23 13:03:14 +0100

    Add:practice/src/functions/PrimeCheck.java

--- practice/src/functions/PrimeCheck.java
+++ practice/src/functions/PrimeCheck.java
@@ -1,4 +1,31 @@
 package functions;
+import java.util.Scanner;
 
 public class PrimeCheck {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is prime");
+        } else {
+            System.out.println(number + " is not prime");
+        }
+
+        input.close();
+    }
 }
