f7766f6b7dcdc089f15b3f1b3f8153d94d089d5a
YOUSSEF ADARDOUR <youssefredmi599@gmail.com>
2026-04-23 13:03:27 +0100

    Add:practice/src/functions/ReverseString.java

--- practice/src/functions/ReverseString.java
+++ practice/src/functions/ReverseString.java
@@ -1,4 +1,35 @@
 package functions;
+import java.util.Scanner;
 
 public class ReverseString {
+
+    public static String reverse(String str) {
+        char[] arr = str.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String userInput = input.nextLine();
+
+        String reversed = reverse(userInput);
+        System.out.println("Reversed string: " + reversed);
+
+        input.close();
+    }
 }
